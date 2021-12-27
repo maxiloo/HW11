617e6803be206072f047b1e40abd1b17a5a11486
Max Unterbusch <max@MacBook-Pro-von-Max.home>
2021-12-27 12:50:59 +0100

    add: test code

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Code at line 6");
 		System.out.println("Code at line 7 added in Branch_1");
+		System.out.println("Test change");
 	}
 	
 }
